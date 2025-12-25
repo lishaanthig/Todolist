042608b5a7584424926b5141ee3df21e2b3b2922
Lishaanthi <lishaanthig@gmail.com>
2025-12-25 15:52:17 +0530

    Create Todolist.js

---
+++ Todolist.js
@@ -0,0 +1,15 @@
+<h1>Industrial Visit List</h1>
+<input></input>
+<button onclick="add()">Add</button>
+<ul id="list">
+    <li>akash</li>
+    <button>delete</button>
+
+</ul>
+<script>
+    var ul=document.getElementById("list")
+    function add()
+    {
+
+    }
+</script>
